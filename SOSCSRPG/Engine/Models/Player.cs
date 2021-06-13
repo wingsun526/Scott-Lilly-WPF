dae8a683c6ef3010bdd639b5cb0091a0059f15fd
Marco Man <wingsun526@gmail.com>
2021-06-13 15:17:13 -0400

    new application file added

---
+++ SOSCSRPG/Engine/Models/Player.cs
@@ -0,0 +1,12 @@
+﻿namespace Engine.Models
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public string CharacterClass { get; set; }
+        public int HitPoints { get; set; }
+        public int ExperiencePoints { get; set; }
+        public int Level { get; set; }
+        public int Gold { get; set; }
+    }
+}
